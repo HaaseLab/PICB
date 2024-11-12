5fd726194b53b26fe7267a10fc1849097536e5e0
Franziska-Marie Ahrend <franziska@ahrend.de>
2024-11-12 10:56:11 -0500

    Add optional seed/core stats & library size
    PICBoptimize: Statistics about seeds and cores are not provided by default but can be obtained by setting PROVIDE.INFO.SEEDS.AND.CORES = TRUE. LIBRARY.SIZE is calculated by default, but can also be provided by the user (e.g., useful for piCB-demo)

--- R/PICBoptimize.R
+++ R/PICBoptimize.R
@@ -2,7 +2,10 @@
 #'
 #' @param IN.ALIGNMENTS list of alignments from PICBload
 #' @param REFERENCE.GENOME name of genome. For example "BSgenome.Dmelanogaster.UCSC.dm6"
+#' @param LIBRARY.SIZE number of reads in the library. By default computed as number of unique mapping alignments + number of primary multimapping alignments.
 #' @param VERBOSITY verbosity level 0/1/2/3. 2 by default.
+#' @param PROVIDE.INFO.SEEDS.AND.CORES FALSE by default.
+#' @param SEQ.LEVELS.STYLE style of chromosome names for BSgenome. "UCSC" by default.
 #' @param ... rest of the parameters used by PICBbuild and provided as iteratable vectors
 #'
 #' @author Aleksandr Friman
@@ -13,7 +16,9 @@
 PICBoptimize <- function(
     IN.ALIGNMENTS,
     REFERENCE.GENOME,
+    LIBRARY.SIZE=length(IN.ALIGNMENTS$unique)+length(IN.ALIGNMENTS$multi.primary),
     VERBOSITY=2,
+    PROVIDE.INFO.SEEDS.AND.CORES=FALSE,
     SEQ.LEVELS.STYLE="UCSC",
     ...
 ){
@@ -62,11 +67,11 @@ PICBoptimize <- function(
   if (is.null(ArgsToIterate)){
     stop("Provide arguments to iterave over. See example.")
   }
-  totalReads=length(IN.ALIGNMENTS$unique) + length(IN.ALIGNMENTS$multi.primary)
+  totalReads=LIBRARY.SIZE
   if (length(unique(c(names(IN.ALIGNMENTS$unique),
                       names(IN.ALIGNMENTS$multi.primary)))
   ) != totalReads){
-    warning("Total number of primary aligments is not equal to total number of read names. Numbers of explained reads may be wrong.")   
+    warning("The total number of primary alignments is not equal to the total number of read names. This discrepancy may occur when using a subset of data, such as in the piCB demonstration, where you specified the LIBRARY.SIZE but the actual number of reads is reduced. For full piRNA datasets, this indicates a potential issue with read assignment and your numbers of explained reads may be wrong.")   
   }
   #building the parameter combinations
   if (VERBOSITY >0 ){
@@ -103,14 +108,20 @@ PICBoptimize <- function(
     #adding columns we iterate over
     outDF[[argName]]=NA
   }
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0("number.of.", locustype)]] = NA
-  }
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0(locustype, ".total.width")]] = NA
-  }
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0("reads.explained.by.", locustype)]] = NA
+  if (PROVIDE.INFO.SEEDS.AND.CORES) {
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0("number.of.", locustype)]] = NA
+    }
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0(locustype, ".total.width")]] = NA
+    }
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0("reads.explained.by.", locustype)]] = NA
+    }
+  } else {
+    outDF[[paste0("number.of.clusters")]] = NA
+    outDF[[paste0("clusters.total.width")]] = NA
+    outDF[[paste0("reads.explained.by.clusters")]] = NA
   }
   #running PICBbuild for every possible combation
   i=1
@@ -133,11 +144,17 @@ PICBoptimize <- function(
     if (VERBOSITY >1 ){
       message("PICBbuild done")
     }
-    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-      outDF[[paste0("number.of.", locustype)]][i]= length(tmrLoci[[locustype]])
-      outDF[[paste0(locustype, ".total.width")]][i]= as.numeric(sum(GenomicRanges::width(tmrLoci[[locustype]])))
-      outDF[[paste0("reads.explained.by.", locustype)]][i]= as.numeric(numberOfallReadsExplained(tmrLoci[[locustype]], IN.ALIGNMENTS.ALL))
-      
+    if (PROVIDE.INFO.SEEDS.AND.CORES) {
+      for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+        outDF[[paste0("number.of.", locustype)]][i]= length(tmrLoci[[locustype]])
+        outDF[[paste0(locustype, ".total.width")]][i]= as.numeric(sum(GenomicRanges::width(tmrLoci[[locustype]])))
+        outDF[[paste0("reads.explained.by.", locustype)]][i]= as.numeric(numberOfallReadsExplained(tmrLoci[[locustype]], IN.ALIGNMENTS.ALL))
+      }
+    } else {
+        outDF[["number.of.clusters"]][i]= length(tmrLoci[[allalignments]])
+        outDF[["clusters.total.width"]][i]= as.numeric(sum(GenomicRanges::width(tmrLoci[[allalignments]])))
+        outDF[["reads.explained.by.clusters"]][i]= as.numeric(numberOfallReadsExplained(tmrLoci[[allalignments]], IN.ALIGNMENTS.ALL))
+
     }
     i=i+1
   }
@@ -146,22 +163,34 @@ PICBoptimize <- function(
   if (VERBOSITY >0 ){
     message("Values normalization")
   }
-
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0("fraction.of.library.explained.by.", locustype)]]= outDF[[paste0("reads.explained.by.", locustype)]]/totalReads
-  }
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0(locustype,".mean.RPKM")]]= outDF[[paste0("reads.explained.by.", locustype)]]*1e9/(outDF[[paste0(locustype, ".total.width")]]*totalReads)
-    outDF[[paste0(locustype,".mean.RPKM")]][ is.na(outDF[[paste0(locustype,".mean.RPKM")]])] = 0
-  }
-  if (typeof(REFERENCE.GENOME)=="character"){
-    SI<-PICBgetchromosomes(REFERENCE.GENOME, SEQ.LEVELS.STYLE)
-  }else{
-    SI<-REFERENCE.GENOME
-  }
-  GenomeSize=sum(GenomeInfoDb::seqlengths(SI))
-  for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
-    outDF[[paste0(locustype, ".fraction.of.genome.space")]]=outDF[[paste0(locustype, ".total.width")]]/(2*GenomeSize)
+  if (PROVIDE.INFO.SEEDS.AND.CORES) {
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0("fraction.of.library.explained.by.", locustype)]]= outDF[[paste0("reads.explained.by.", locustype)]]/totalReads
+    }
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0(locustype,".mean.RPKM")]]= outDF[[paste0("reads.explained.by.", locustype)]]*1e9/(outDF[[paste0(locustype, ".total.width")]]*totalReads)
+      outDF[[paste0(locustype,".mean.RPKM")]][ is.na(outDF[[paste0(locustype,".mean.RPKM")]])] = 0
+    }
+    if (typeof(REFERENCE.GENOME)=="character"){
+      SI<-PICBgetchromosomes(REFERENCE.GENOME, SEQ.LEVELS.STYLE)
+    }else{
+      SI<-REFERENCE.GENOME
+    }
+    GenomeSize=sum(GenomeInfoDb::seqlengths(SI))
+    for (locustype in c(uniqueonly,uniqueandprimary,allalignments )){
+      outDF[[paste0(locustype, ".fraction.of.genome.space")]]=outDF[[paste0(locustype, ".total.width")]]/(2*GenomeSize)
+    }
+  } else {
+      outDF[["fraction.of.library.explained.by.clusters"]]= outDF[["reads.explained.by.clusters"]]/totalReads
+      outDF[["clusters.mean.RPKM"]]= outDF[["reads.explained.by.clusters"]]*1e9/(outDF[["clusters.total.width"]]*totalReads)
+      outDF[["clusters.mean.RPKM"]][ is.na(outDF[["clusters.mean.RPKM"]])] = 0
+      if (typeof(REFERENCE.GENOME)=="character"){
+        SI<-PICBgetchromosomes(REFERENCE.GENOME, SEQ.LEVELS.STYLE)
+      }else{
+        SI<-REFERENCE.GENOME
+      }
+      GenomeSize=sum(GenomeInfoDb::seqlengths(SI))
+      outDF[["clusters.fraction.of.genome.space"]]=outDF[["clusters.total.width"]]/(2*GenomeSize)
   }
   return(outDF)
 }
