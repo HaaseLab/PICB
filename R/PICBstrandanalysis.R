c769fabb64e369dbfddcc5b60a46c7dfde992581
Franziska-Marie Ahrend <franziska@ahrend.de>
2024-11-27 15:07:37 -0500

    Chore: PICBstrandanalysis tests
    Increase robustness and include testthat for PICBstrandanalysis and PICBloadfasta.

--- R/PICBstrandanalysis.R
+++ R/PICBstrandanalysis.R
@@ -1,4 +1,4 @@
-#' Add sense/antisense ratio of piRNAs per piRNAcluster
+#' Add sense/antisense ratio of unique piRNAs per piRNAcluster
 #'
 #' @param IN.ALIGNMENTS list of alignments from PICBload
 #' @param IN.RANGES single GRanges object (seeds, cores or clusters from PICBbuild)
@@ -29,10 +29,10 @@ PICBstrandanalysis<-function(IN.ALIGNMENTS,
                              IN.RANGES,
                              VERBOSE=TRUE){
     
-    if(is.null(IN.ALIGNMENTS)) stop("Please provide IN.ALIGNMENTS !")
-    if(is.null(IN.RANGES)) stop("Please provide IN.RANGES !")
-    if(is.list(IN.RANGES)) stop("IN.RANGES must be a single GRanges object (seeds, cores or clusters)!")
-
+    if(is.null(IN.ALIGNMENTS) || length(IN.ALIGNMENTS$unique) == 0) stop("IN.ALIGNMENTS is either NULL or contains no rows. Please provide valid IN.ALIGNMENTS from PICBload!")
+    if(is.null(IN.RANGES) || length(IN.RANGES)==0) stop("IN.RANGES is either NULL or contains no rows. Use the GRanges object seeds, cores, or clusters from PICBbuild to create valid IN.RANGES!")
+    if(is.list(IN.RANGES)) stop("IN.RANGES must be a single GRanges object: seeds, cores, or clusters from PICBbuild!")
+    
 
     if (VERBOSE==TRUE) message("PICB v", utils::packageVersion("PICB")," Starting with strand analysis ... ")
 
@@ -42,18 +42,27 @@ PICBstrandanalysis<-function(IN.ALIGNMENTS,
     # get strandedness
     IN.ALIGNMENTS.PLUS <- IN.ALIGNMENTS[GenomicRanges::strand(IN.ALIGNMENTS) == "+"]
     IN.ALIGNMENTS.MINUS <- IN.ALIGNMENTS[GenomicRanges::strand(IN.ALIGNMENTS) == "-"]
-
+    
     if (VERBOSE==TRUE) message("\tFinding overlaps of clusters with piRNAs from plus strand")
     DT_plus <- as.data.frame(GenomicRanges::findOverlaps(IN.RANGES, IN.ALIGNMENTS.PLUS, ignore.strand = TRUE))
-    DT_plus.2 <- stats::aggregate(subjectHits ~ queryHits, data = DT_plus, FUN = length)
+    if (nrow(DT_plus) == 0) {
+        warning("No overlaps detected between IN.ALIGNMENTS and clusters on the plus strand. Please verify that IN.ALIGNMENTS and IN.RANGES are valid and correctly correspond to each other. Continuing ...")
+        DT_plus.2 <- data.frame(queryHits = integer(0), subjectHits = integer(0))
+    } else {
+        DT_plus.2 <- stats::aggregate(subjectHits ~ queryHits, data = DT_plus, FUN = length)
+    }
     # Add numbers of plus-strand piRNAs to clusters
     IN.RANGES$plus_piRNAs <- 0
     IN.RANGES$plus_piRNAs[DT_plus.2$queryHits] <- DT_plus.2$subjectHits
 
     if (VERBOSE==TRUE) message("\tFinding overlaps of clusters with piRNAs from minus strand")
     DT_minus <- as.data.frame(GenomicRanges::findOverlaps(IN.RANGES, IN.ALIGNMENTS.MINUS, ignore.strand = TRUE))
-    DT_minus.2 <- stats::aggregate(subjectHits ~ queryHits, data = DT_minus, FUN = length)
-    
+    if (nrow(DT_minus) == 0) {
+        warning("No overlaps detected between IN.ALIGNMENTS and clusters on the minus strand. Please verify that IN.ALIGNMENTS and IN.RANGES are valid and correctly correspond to each other. Continuing ...")
+        DT_minus.2 <- data.frame(queryHits = integer(0), subjectHits = integer(0))
+    } else {
+        DT_minus.2 <- stats::aggregate(subjectHits ~ queryHits, data = DT_minus, FUN = length)
+    }
     # Add numbers of plus-strand piRNAs to clusters
     IN.RANGES$minus_piRNAs <- 0
     IN.RANGES$minus_piRNAs[DT_minus.2$queryHits] <- DT_minus.2$subjectHits
---
+++ tests/testthat/test-PICBstrandanalysis.R
@@ -0,0 +1,145 @@
+#expect_equal(myFunction(testData), expectedOutput)
+
+#invalid IN.ALIGNMENTS
+test_that("PICBstrandanalysis stops with invalid IN.ALIGNMENTS", {
+  alignments <- list(unique = GenomicRanges::GRanges())
+  ranges <- GenomicRanges::GRanges()
+
+  expect_error(PICBstrandanalysis(NULL, ranges), "IN.ALIGNMENTS is either NULL or contains no rows. Please provide valid IN.ALIGNMENTS from PICBload!")
+})
+
+#invalid IN.RANGES
+test_that("PICBstrandanalysis stops with invalid IN.RANGES", {
+  alignments <- list(unique = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1"),
+    ranges = IRanges::IRanges(start = c(1000, 2000), width = 50),
+    strand = c("+", "-"),
+    NH = c(1, 1)
+  ))
+  ranges <- GenomicRanges::GRanges()
+  expect_error(PICBstrandanalysis(alignments, NULL), "IN.RANGES is either NULL or contains no rows. Use the GRanges object seeds, cores, or clusters from PICBbuild to create valid IN.RANGES!")
+  expect_error(PICBstrandanalysis(alignments, ranges), "IN.RANGES is either NULL or contains no rows. Use the GRanges object seeds, cores, or clusters from PICBbuild to create valid IN.RANGES!")
+  expect_error(PICBstrandanalysis(alignments, list(ranges)), "IN.RANGES must be a single GRanges object: seeds, cores, or clusters from PICBbuild!")
+  
+  })
+
+#valid IN.ALIGNMENTS and IN.RANGES expect_equal
+test_that("PICBstrandanalysis returns expected output when alignments on both strands", {
+  alignments <- list(unique = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1", "chr1", "chr1", "chr1", "chr1", "chr1", "chr1"),
+    ranges = IRanges::IRanges(start = c(15, 27, 30, 19, 43, 52, 75, 80), width = c(26, 27, 27, 27, 27, 26, 27, 27)),
+    strand = c("+", "+", "+", "+", "-", "-", "-", "-"),
+    NH = c(1, 1, 1, 1, 1, 1, 1, 1)
+  ), multi.primary = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1", "chr1", "chr1", "chr1", "chr1"), 
+    ranges = IRanges::IRanges(start = c(20, 30, 40, 60, 68, 81), width = c(26, 27, 27, 27, 27, 27)), 
+    strand = c("+", "-", "-", "-", "+", "-"), 
+    NH = c(3, 2, 4, 100, 34, 90)
+  ))
+
+  ranges <- GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1"), 
+    ranges = IRanges::IRanges(start = c(10, 40), width = c(40, 60)), 
+    strand = c("+", "-")
+  )
+  #piC on plus strand: sense/antisense = 4/1;
+  #piC on minus strand: sense/antisense = 4/4;
+
+  # Note: With piRNA input, we anticipate a large number of alignments. 
+  # To prevent division by zero when calculating the sense-to-antisense ratio (s_as_ratio), we add a pseudocount of 1 to both the sense and antisense counts. 
+  # Consequently, the expected value in the s_as_ratio column is adjusted to 2.5 and 1.0 instead of 4.0 and 1.0 in this mock dataset. 
+  # This adjustment ensures numerical stability during computation and does not affect the accuracy of results when working with real piRNA data, where counts are sufficiently large.
+
+  #piC on plus strand: sense+1/antisense+1 = 5/2 = 2.5
+  #piC on minus strand: sense+1/antisense+1 = 5/5 = 1.0
+
+  expect_equal(PICBstrandanalysis(alignments, ranges), 
+    GenomicRanges::GRanges(
+      seqnames = c("chr1", "chr1"), 
+      ranges = IRanges::IRanges (start = c(10, 40), width = c(40, 60)), 
+      strand = c("+", "-"),
+      s_as_ratio = c(2.5, 1.0)
+    )
+  )
+
+})
+
+
+#valid IN.ALIGNMENTS and IN.RANGES but just one strand in IN.RANGES, expect_equal
+test_that("PICBstrandanalysis returns expected output when no clusters on plus strand", {
+  alignments <- list(unique = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1", "chr1", "chr1", "chr1", "chr1", "chr1", "chr1"),
+    ranges = IRanges::IRanges(start = c(15, 27, 30, 19, 43, 52, 75, 80), width = c(26, 27, 27, 27, 27, 26, 27, 27)),
+    strand = c("+", "+", "+", "+", "-", "-", "-", "-"),
+    NH = c(1, 1, 1, 1, 1, 1, 1, 1)
+  ), multi.primary = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1", "chr1", "chr1", "chr1", "chr1"), 
+    ranges = IRanges::IRanges(start = c(20, 30, 40, 60, 68, 81), width = c(26, 27, 27, 27, 27, 27)), 
+    strand = c("+", "-", "-", "-", "+", "-"), 
+    NH = c(3, 2, 4, 100, 34, 90)
+  ))
+
+  ranges <- GenomicRanges::GRanges(
+    seqnames = c("chr1"), 
+    ranges = IRanges::IRanges(start = c(10), width = c(40)), 
+    strand = c("+")
+  )
+
+  expect_equal(PICBstrandanalysis(alignments, ranges), 
+    GenomicRanges::GRanges(
+      seqnames = c("chr1"), 
+      ranges = IRanges::IRanges(start = c(10), width = c(40)), 
+      strand = c("+"),
+      s_as_ratio = c(2.5)
+    ))
+  
+  ranges <- GenomicRanges::GRanges(
+    seqnames = c("chr1"), 
+    ranges = IRanges::IRanges(start = c(40), width = c(60)), 
+    strand = c("-")
+  )
+
+  expect_equal(PICBstrandanalysis(alignments, ranges), 
+    GenomicRanges::GRanges(
+      seqnames = c("chr1"), 
+      ranges = IRanges::IRanges(start = c(40), width = c(60)), 
+      strand = c("-"),
+      s_as_ratio = c(1.0)
+    ))
+
+})
+
+#valid IN.RANGES but IN.ALIGNMENTS not corresponding to IN.RANGES, expect_equal
+test_that("PICBstrandanalysis returns expected output when IN.ALIGNMENTS not corresponding to IN.RANGES", {
+  alignments <- list(unique = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1", "chr1", "chr1"),
+    ranges = IRanges::IRanges(start = c(15, 27, 30, 19), width = c(26, 27, 27, 27)),
+    strand = c("+", "+", "+", "+"),
+    NH = c(1, 1, 1, 1)
+  ), multi.primary = GenomicRanges::GRanges(
+    seqnames = c("chr1", "chr1"), 
+    ranges = IRanges::IRanges(start = c(20, 68), width = c(26, 27)), 
+    strand = c("+", "+"), 
+    NH = c(3, 34)
+  ))
+
+  ranges <- GenomicRanges::GRanges(
+    seqnames = c("chr1"), 
+    ranges = IRanges::IRanges(start = c(40), width = c(60)), 
+    strand = c("-")
+  )
+
+  result <- expect_warning(
+    PICBstrandanalysis(alignments, ranges), 
+    "No overlaps detected between IN.ALIGNMENTS and clusters on the minus strand. Please verify that IN.ALIGNMENTS and IN.RANGES are valid and correctly correspond to each other. Continuing ..."
+  )
+  #piC on minus strand: sense+1/antisense+1 = 1/5 = 0.2
+  expect_equal(result, 
+    GenomicRanges::GRanges(
+      seqnames = c("chr1"), 
+      ranges = IRanges::IRanges(start = c(40), width = c(60)), 
+      strand = c("-"),
+      s_as_ratio = c(0.2)
+    ))
+
+})
